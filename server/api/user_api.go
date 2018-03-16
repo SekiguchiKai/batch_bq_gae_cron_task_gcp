62bd62fccdcceebf1a6d1cfd8da7cf60904e71e9
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-16 18:04:43 +0900

    HTTPのリクエストボディのjsonデータUserに変換するbindUserFromJsonを作成した

--- server/api/user_api.go
+++ server/api/user_api.go
@@ -1,11 +1,24 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/model"
+)
 
 func createUser(c *gin.Context) {
 
 }
 
+
+// HTTPのリクエストボディのjsonデータUserに変換する。
+func bindUserFromJson(c *gin.Context, dst *model.User) error {
+	if err := c.BindJSON(dst); err != nil {
+		return err
+	}
+
+	dst.ID = getUserID(c)
+	return nil
+}
 
 // URIのIDを取得する。
 func getUserID(c *gin.Context) string {
