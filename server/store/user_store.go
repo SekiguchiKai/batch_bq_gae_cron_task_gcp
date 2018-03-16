2bd3128f2051aeff979e5df83eb2d7f63e8b1673
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-16 17:59:18 +0900

    DatastoreにUserを格納するPutUserを作成した

--- server/store/user_store.go
+++ server/store/user_store.go
@@ -5,6 +5,8 @@ import (
 	"google.golang.org/appengine"
 	"net/http"
 	"google.golang.org/appengine/datastore"
+	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/model"
+	"ana-ccs/glm/server/util"
 )
 
 const _UserKind = "User"
@@ -18,11 +20,22 @@ type UserStore struct {
 func NewUserStore(r *http.Request) UserStore {
 	return NewUserStoreWithContext(appengine.NewContext(r))
 }
-
-
 // context.ContextからUserStoreを新規発行する。
 func NewUserStoreWithContext(ctx context.Context) UserStore {
 	return UserStore{ctx: ctx}
+}
+
+// DatastoreにUserを格納する。
+func (s UserStore)PutUser(u model.User)error {
+	util.InfoLogWithContext(s.ctx, "PutUser is called")
+
+	key := s.newUserStoreKey(u.ID)
+
+	if _, err :=  datastore.Put(s.ctx, key, u); err != nil {
+		return err
+	}
+
+
 }
 
 // UserKind用のdatastore.Keyを発行する。
