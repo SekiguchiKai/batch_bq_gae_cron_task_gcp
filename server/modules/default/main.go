51919b781dd77c7e7e545361e9b9e704722d8c09
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-16 18:44:45 +0900

    エントリポイントであるinitを作成した

--- server/modules/default/main.go
+++ server/modules/default/main.go
@@ -8,6 +8,15 @@ import (
 
 const _APIPath = "/api"
 
+// エントリポイント
+func init() {
+	g := gin.New()
+	initAPI(g)
+	// gin.New()の戻り値のEngineは、ServeHTTP(ResponseWriter, *Request)メソッドを持っているので、
+	// type Handler interfaceを満たす
+	http.Handle("/", g)
+}
+
 // API群を初期登録する。
 func initAPI(g *gin.Engine) {
 	apiGin := g.Group(_APIPath)
