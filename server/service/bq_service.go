06b387ce3ee8d3509e973dee135325a7951ec042
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-16 17:41:06 +0900

    BQClientWrapperを生成するNewBQClientWrapperを作成した

--- server/service/bq_service.go
+++ server/service/bq_service.go
@@ -10,3 +10,14 @@ type BQClientWrapper struct {
 	ctx    context.Context
 	client *bigquery.Client
 }
+
+// BQClientWrapperを生成する。
+func NewBQClientWrapper(ctx context.Context, prjID string) (BQClientWrapper, error) {
+	// context.Contextとappengine.AppID(project ID)からBigQueryのClientLibraryを作成した
+	client, err := bigquery.NewClient(ctx, prjID)
+	if err != nil {
+		return BQClientWrapper{}, err
+	}
+
+	return BQClientWrapper{client: client}, nil
+}
