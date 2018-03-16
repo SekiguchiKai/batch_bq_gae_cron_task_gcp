331d9e5b37b30be3f4174c48f12b203e664494cd
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-16 17:41:56 +0900

    指定したBigQueryのDataset.TableにデータをアップロードするPutDataを作成した

--- server/service/bq_service.go
+++ server/service/bq_service.go
@@ -11,7 +11,7 @@ type BQClientWrapper struct {
 	client *bigquery.Client
 }
 
-// BQClientWrapperを生成する。
+// BQClientWrapperを生成する
 func NewBQClientWrapper(ctx context.Context, prjID string) (BQClientWrapper, error) {
 	// context.Contextとappengine.AppID(project ID)からBigQueryのClientLibraryを作成した
 	client, err := bigquery.NewClient(ctx, prjID)
@@ -20,4 +20,16 @@ func NewBQClientWrapper(ctx context.Context, prjID string) (BQClientWrapper, err
 	}
 
 	return BQClientWrapper{client: client}, nil
+}
+
+// 指定したBigQueryのDataset.Tableにデータをアップロードする。
+func (bq *BQClientWrapper) PutData(dataset, table string, src interface{}) error {
+
+	// BigQueryの指定したDataset.TableのUploaderを作成
+	upl := bq.client.Dataset(dataset).Table(table).Uploader()
+	// 不適切なデータが含まれていた場合に、無視する
+	upl.SkipInvalidRows = true
+
+	// アップロードする
+	return upl.Put(bq.ctx, src)
 }
