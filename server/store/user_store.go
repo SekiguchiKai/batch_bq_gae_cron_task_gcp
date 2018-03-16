a54268eb70b43f5a458b1fbd0aec2df76bbabec4
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-16 17:54:56 +0900

    http.RequestからUserStoreを新規発行するNewUserStoreを作成した

--- server/store/user_store.go
+++ server/store/user_store.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"context"
+	"google.golang.org/appengine"
+	"net/http"
 )
 
 const _UserKind = "User"
@@ -9,6 +11,11 @@ const _UserKind = "User"
 // User用のUserStore
 type UserStore struct {
 	ctx context.Context
+}
+
+// http.RequestからUserStoreを新規発行する。
+func NewUserStore(r *http.Request) UserStore {
+	return NewUserStoreWithContext(appengine.NewContext(r))
 }
 
 
