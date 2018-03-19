0ca495c8fbe5a4ffb776e134966cd2abfc63460b
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-19 18:48:58 +0900

    server_test.shを各ディレクトリからテストするように変更した

--- scripts/server_test.sh
+++ scripts/server_test.sh
@@ -1,4 +1,28 @@
 #! /bin/sh
 
-cd $GOPATH/src/github.com/SekiguchiKai/batch_bq_task_gcp/server
-goapp test ./modules/default
+cur=`pwd`
+# コマンドライン引数で、directory指定
+cd $1
+testdirs=`ls -d */`
+STAT=0
+
+for testdir in $testdirs
+do
+  if [ $testdir = "vendor/" ] || [ $testdir = "modules/" ]; then
+    continue
+  fi
+  cd $testdir
+  goapp test -v -cover ./
+  if [ $? -ne 0 ]; then
+    echo "Test is failed in $testdir"
+    STAT=1
+  fi
+  # コマンドライン引数で、directory指定
+  cd $1
+done
+cd $cur
+
+if [ $STAT -eq 1 ]; then
+  echo "Server Test is failed"
+  exit $STAT
+fi
