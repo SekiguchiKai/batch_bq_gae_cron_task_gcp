065a44cba659fd0e4af7850e96a17d25f368baf9
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-23 14:16:24 +0900

    backend module用のエントリポイントを作成した

--- server/modules/backend/main.go
+++ server/modules/backend/main.go
@@ -4,10 +4,14 @@ import (
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/task"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/util"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+// エントリポイント
 func init() {
-
+	g := gin.New()
+	initTaskAPI(g)
+	http.Handle("/", g)
 }
 
 // Task API群を初期登録する。
