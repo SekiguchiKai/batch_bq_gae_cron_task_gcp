5636f763e4656c96a723e65566af68394e8915a1
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-26 18:36:01 +0900

    server_setup.shのディレクトリを修正した

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 go get -u github.com/golang/dep/cmd/dep
-cd $GOPATH/src/github.com/SekiguchiKai/batch_bq_task_gcp/server
+cd $GOPATH/src/github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server
 dep ensure
