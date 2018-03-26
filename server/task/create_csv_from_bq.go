0ef7aff1400556299a0fffb1bddbbcc60b32a10e
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-26 17:38:54 +0900

    createCsvFromBigQueryをTaskQueueのTaskに追加するstartCreateCsvFromBigQueryを作成した

--- server/task/create_csv_from_bq.go
+++ server/task/create_csv_from_bq.go
@@ -1,16 +1,36 @@
 package task
 
 import (
+	"context"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/model"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/service"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/util"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/taskqueue"
+	"net/url"
 )
 
+// createCsvFromBigQueryをTaskQueueのTaskに追加する
+func startCreateCsvFromBigQuery(ctx context.Context, queueName, sql string) error {
+	util.InfoLogWithContext(ctx, "startCreateCsvFromBigQuery is called")
+
+	taskPath := util.GetTaskPath()
+	t := taskqueue.NewPOSTTask(taskPath+"/createCsvFromBigQuery", url.Values{
+		"": []string{sql},
+	})
+
+	if _, err := taskqueue.Add(ctx, t, queueName); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
 // BigQueryからデータを抽出し、抽出したデータからCSVを作成する
-func createCsvFromBigquery(c *gin.Context, sql string) error {
-	util.InfoLog(c.Request, "createCsvFromBigquery is called")
+func createCsvFromBigQuery(c *gin.Context, sql string) error {
+	util.InfoLog(c.Request, "createCsvFromBigQuery is called")
 
 	ctx := appengine.NewContext(c.Request)
 	prjID := appengine.AppID(ctx)
