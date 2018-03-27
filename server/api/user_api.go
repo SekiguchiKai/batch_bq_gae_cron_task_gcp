63fb9bfa8bce5f0d46c5f8dd874d5f403f3ba6a5
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-03-27 10:54:13 +0900

    リクエストを元にBigQueryからcsvを作成するcreateUserAnalyzedResultを作成した

--- server/api/user_api.go
+++ server/api/user_api.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/model"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/store"
+	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/task"
 	"github.com/SekiguchiKai/batch_bq_gae_cron_task_gcp/server/util"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"google.golang.org/appengine"
 	"net/http"
 	"strconv"
 	"time"
@@ -22,11 +25,33 @@ const (
 // UserAPIを初期化する。
 func InitUserAPI(g *gin.RouterGroup) {
 	g.POST("/user/new", createUser)
+	g.POST("user/analysis", createUserAnalyzedResult)
 
 }
 
+// リクエストを元にBigQueryからcsvを作成する
 func createUserAnalyzedResult(c *gin.Context) {
 	util.InfoLog(c.Request, "createUserAnalyzedResult is called")
+
+	var param model.UserForAnalyze
+	if err := bindUserForAnalyzeFromJson(c, &param); err != nil {
+		util.RespondAndLog(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	sql, err := createSQLFromUserForAnalyze(param)
+	if err != nil {
+		util.RespondAndLog(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	ctx := appengine.NewContext(c.Request)
+	if err := task.StartCreateCsvFromBigQuery(ctx, sql); err != nil {
+		util.RespondAndLog(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
 
 }
 
@@ -88,7 +113,7 @@ func bindUserFromJson(c *gin.Context, dst *model.User) error {
 	return nil
 }
 
-// HTTPのリクエストボディのjsonデータUserに変換する。
+// HTTPのリクエストボディのjsonデータUserForAnalyzeに変換する。
 func bindUserForAnalyzeFromJson(c *gin.Context, dst *model.UserForAnalyze) error {
 	if err := c.BindJSON(dst); err != nil {
 		return err
@@ -104,11 +129,11 @@ func getUserID(c *gin.Context) string {
 
 // 送信されて来たUserに必要なデータが存在するかどうかのバリデーションを行う。
 func validateParamsForUser(u model.User) error {
-	if u.UserName == _EMPTY {
+	if u.UserName == _Empty {
 		return util.CreateErrMessage(_UserName, _RequiredErrMessage)
 	}
 
-	if u.MailAddress == _EMPTY {
+	if u.MailAddress == _Empty {
 		return util.CreateErrMessage(_MailAddress, _RequiredErrMessage)
 	}
 
@@ -116,14 +141,53 @@ func validateParamsForUser(u model.User) error {
 		return util.CreateErrMessage(_Age, _ShouldBeOver, strconv.Itoa(0))
 	}
 
-	if u.Gender == _EMPTY {
+	if u.Gender == _Empty {
 		return util.CreateErrMessage(_Gender, _RequiredErrMessage)
 	}
 
-	if u.From == _EMPTY {
+	if u.From == _Empty {
 		return util.CreateErrMessage(_From, _RequiredErrMessage)
 	}
 
 	return nil
 
+}
+
+// UserForAnalyzeからSQLを作成する
+func createSQLFromUserForAnalyze(u model.UserForAnalyze) (string, error) {
+
+	base := `SELECT *
+FROM [sandbox-sekky0905:batch_bq_task_gcp.user]
+WHERE 
+`
+
+	sql := base
+
+	if u.UserNameField.Signal != "" && u.UserNameField.Value != "" {
+		sql = sql + "UserName" + _Space + u.UserNameField.Signal + _Space + u.UserNameField.Value
+	}
+	if u.MailAddressField.Signal != "" && u.MailAddressField.Value != "" {
+		sql = sql + "MailAddress" + _Space + u.MailAddressField.Signal + _Space + _DoubleQuotation + u.MailAddressField.Value + _DoubleQuotation
+	}
+	if u.AgeField.Signal != "" && strconv.Itoa(u.AgeField.Value) != "" {
+		sql = sql + "Age" + _Space + u.AgeField.Signal + _Space + _DoubleQuotation + strconv.Itoa(u.AgeField.Value) + _DoubleQuotation
+	}
+	if u.GenderField.Signal != "" && u.GenderField.Value != "" {
+		sql = sql + "Gender" + _Space + u.GenderField.Signal + _Space + _DoubleQuotation + u.GenderField.Value + _DoubleQuotation
+	}
+	if u.FromField.Signal != "" && u.FromField.Value != "" {
+		sql = sql + "From" + _Space + u.FromField.Signal + _Space + _DoubleQuotation + u.FromField.Value + _DoubleQuotation
+	}
+	if u.CreatedAtField.Signal != "" && u.CreatedAtField.Value != "" {
+		sql = sql + "CreatedAt" + _Space + u.CreatedAtField.Signal + _Space + _DoubleQuotation + u.CreatedAtField.Value + _DoubleQuotation
+	}
+	if u.UpdatedAtField.Signal != "" && u.UpdatedAtField.Value != "" {
+		sql = sql + "UpdatedAt" + _Space + u.UpdatedAtField.Signal + _Space + _DoubleQuotation + u.UpdatedAtField.Value + _DoubleQuotation
+	}
+
+	if sql == base {
+		return "", errors.New("There is not value and signal")
+	}
+
+	return sql, nil
 }
